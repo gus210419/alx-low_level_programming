9cf58f3675313354e4c5cebba649e78cc353efdb
Augustus Atuaful <fluteflether@gmail.com>
2023-03-21 15:40:33 -0700

    1-alphabet.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+
+{
+	char f;
+
+	for (f = 'a'; f <= 'z'; f++)
+	_putchar(f);
+
+	_putchar('\n');
+}
+
