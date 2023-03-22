b91ee8fdf1a2812d7e5bb344aa091d22a28703a2
Augustus Atuaful <fluteflether@gmail.com>
2023-03-22 14:36:07 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,10 +5,12 @@
  * @c: is the char to be checked
  * Return: 1 if char is lowercase, otherwise 0.
  */
+
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= '97' && c <= '122')
 		return (1);
 	else
 		return (0);
-}	
+
+}
